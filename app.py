cd820a4adb9904cf5ae46b415150411410380a3d
beth <besspar@gmail.com>
2021-12-12 16:31:48 +0300

    Changed for homework

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+import json
+from functions import *
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
@@ -9,17 +10,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    pass
+    hashtags = get_hashtags()
+    return render_template("index.html", hashtags=hashtags)
 
 
-@app.route("/tag")
+
+@app.route("/tag/")
 def page_tag():
-    pass
+    search = request.args.get("tag")
+    matched_posts = get_posts(search)
+
+    return render_template("post_by_tag.html", posts=matched_posts)
+
+
 
 
 @app.route("/post", methods=["GET", "POST"])
 def page_post_create():
-    pass
+    if request.method == "GET":
+        return render_template("post_form.html")
+    if request.method == "POST":
+        content = request.form["content"]
+        file = request.files['picture']
+        if file:
+            add_post(file, content)
+            return render_template("post_uploaded.html", content=content)
+
 
 
 @app.route("/uploads/<path:path>")
--- functions.py
+++ functions.py
@@ -1,1 +1,57 @@
 import json
+
+
+def get_hashtags():
+    with open("posts.json", "r") as posts_file:
+        posts = json.load(posts_file)
+
+    hashtag_list = []
+    post_letters = ''
+    for item in posts:
+        post_text = item['content']
+        for letter in post_text:
+            if letter not in ['!', ',', '.', '-', '?', '%', '$', '@']:
+                post_letters += letter
+        post_words = post_letters.split()
+        for word in post_words:
+            if word.startswith("#"):
+                hashtag_list.append(word)
+
+    set_of_hashtags = set(hashtag_list)
+    hashtag_list = list(set_of_hashtags)
+
+    final_list = []
+    for hashtag in hashtag_list:
+        if hashtag.startswith("#"):
+            final_list.append(hashtag[1:len(hashtag)])
+
+    final_list.sort()
+    return final_list
+
+
+def get_posts(tag):
+    with open("posts.json", "r") as posts_file:
+        posts = json.load(posts_file)
+
+    posts_list = []
+    for item in posts:
+        if ("#"+tag) in item['content']:
+            posts_list.append(item)
+
+    return posts_list
+
+
+def add_post(file, content):
+    with open("posts.json", "r") as posts_file:
+        posts = json.load(posts_file)
+
+    posts_content = {"pic": "file_path", "content": content}
+
+    posts.append(posts_content)
+
+    with open ("posts.json", "w") as posts_file:
+        json.dump(posts, posts_file, ensure_ascii=False, indent=4)
+
+
+
+
